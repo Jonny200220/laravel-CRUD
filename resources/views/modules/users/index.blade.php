43b0943b7495839dc6d5aed50f0ee8e293a2863b
Jonathan Ramirez Hdz <jonnyrmzhdz@gmail.com>
2024-10-04 00:27:32 -0600

    Agregando layout, y migraciones para la db "primeros pasos"

---
+++ resources/views/modules/users/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts/main')
+
+@section('contenido')
+    <div class="container mt-4 ">
+        <h2>Crud con Laravel 11 - Nombres</h2>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+@endsection
