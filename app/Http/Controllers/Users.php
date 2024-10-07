f433e3c6eeb04356018f3156d5bffc37798751ba
Jonny <jonnyrmzhdz@gmail.com>
2024-10-07 08:29:50 -0600

    Nueva rama, creando vista de funcion create

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -19,7 +19,7 @@ class Users extends Controller
      */
     public function create()
     {
-        //
+        return view('modules/users/create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Users;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [Users::class, 'index'])->name('inedx');
+Route::get('/', [Users::class, 'index'])->name('index');
